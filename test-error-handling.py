768f3e44d1f6206e08d3998daacff60ca8e71b1f
ianrosevear <ian.rosevear@gmail.com>
2021-04-29 17:45:50 -0400

    Added external riddle_functions

--- test-error-handling.py
+++ test-error-handling.py
@@ -4,46 +4,44 @@
 
 ## the requests package is used to make api requests
 import requests
-api_calls = 0
+import riddle_functions as rf
 
-def call_api(s):
-    global api_calls
-    api_calls = api_calls + 1
-    return requests.get(s).json()
+# api calls total
+ac = 0
 
 def print_riddles(has, used, answer):
     return "What has "+ str(has)+\
     " but isn't used for " + str(used['edges'][0]['end']['@id'])+\
      "?\n"+ str(answer) +"!"
+
+
+
 ## hardcode a start word for test purposes
 startword = "dog"
 
-## form is 'http://api.conceptnet.io/c/en/' + word
+## request form is 'http://api.conceptnet.io/c/en/' + word
 ## the form of an @id is /c/en/word
-api = "http://api.conceptnet.io"
-node1 = call_api(api + "/c/en/" + startword)
+ac, node1 = rf.call_api(ac, "w", startword)
+
 #node1_id = node1["@id"]
 #if part of -> used case:
 def find_pair_has(node1, index):
+    global ac
     #checking index so that it doesn't loop forever on words that will not work in this form
     node1_id = node1["@id"]
     while index < 5:
-        query = "/query?" +\
-                "start=" + node1_id +\
-                "&" +\
-                "rel=/r/HasA"
-        node1_has = call_api(api + query)
+        # query for things that node1 has
+        ac, node1_has = rf.call_api(ac, "q", "start="+node1_id, "rel=/r/HasA")
+
         # try except block with recursion: tries the whole process on the first edge for node1_has,
         # and if this gives an exception in the end, tries again with index 2. Up to 5.
         try:
             # next we get the id of something that node1 has
             node2_hasid = node1_has['edges'][index]['end']['@id']
+
             # then we query for things that also have node2
-            query = "/query?" +\
-                    "end=" + node2_hasid +\
-                    "&" +\
-                    "rel=/r/HasA"
-            has_node2 = call_api(api + query)
+            ac, has_node2 = rf.call_api(ac, "q", "end="+node2_hasid, "rel=/r/HasA")
+
             # next we get the id of something that has node2
             # and is dissimilar to node1
             # we can't do this too many times lest we overstep the limits
@@ -54,19 +52,15 @@ def find_pair_has(node1, index):
             node3_id = ""
             for n in has_node2['edges'][0:max_lookups]:
                 curr_id = n['start']['@id']
-                related_lookup = "/relatedness?" +\
-                                 "node1=" + node1_id +\
-                                 "&" +\
-                                 "node2=" + curr_id
-                relatedness = call_api(api + related_lookup)['value']
+
+                ac, relatedness = rf.call_api(ac, "r", "node1="+node1_id, "node2="+curr_id)
+
                 if min_similarity < relatedness < max_similarity:
                     node3_id = curr_id
                     break
-            query = "/query?" +\
-                    "start=" + node3_id +\
-                    "&" +\
-                    "rel=/r/UsedFor"
-            node3_used = call_api(api + query)
+
+            ac, node3_used = rf.call_api(ac, "q", "start="+node3_id, "rel=/r/UsedFor")
+
             output_string = print_riddles(node2_hasid, node3_used, node1_id)
         #if there is an error, try again with index 1 higher
         except:
@@ -77,4 +71,4 @@ def find_pair_has(node1, index):
 
 #returns none for some reason?? can't figure out why
 print(find_pair_has(node1, 0))
-print("\nAPI calls:", api_calls)
+print("\nAPI calls (counting relatedness as 2):", ac)
