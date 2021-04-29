219e9d133328a6287784e99077d320a3e0b6b73e
Celia Morral <cmorral@boole.cs.vassar.edu>
2021-04-29 13:07:18 -0400

    added partof to original file, added possible way to handle errors (although not functional yet

--- conceptnet-test.py
+++ conceptnet-test.py
@@ -13,7 +13,7 @@ def call_api(s):
 
 
 ## hardcode a start word for test purposes
-startword = "keyboard"
+startword = "key"
 
 ## form is 'http://api.conceptnet.io/c/en/' + word
 api = "http://api.conceptnet.io"
@@ -30,30 +30,40 @@ node1_id = node1["@id"]
 # "start=" + node_id --> start the edge from 'node1_id' node
 # "&" --> connect the parameters
 # "rel=/r/HasA" --> edge has relation type "HasA"
+##query = "/query?" +\
+##        "start=" + node1_id +\
+##        "&" +\
+##        "rel=/r/HasA"
+##node1_has = call_api(api + query)
+
+
+## repeat process but for /r/PartOf (to be used in a sentence "what is part of__"
 query = "/query?" +\
         "start=" + node1_id +\
         "&" +\
-        "rel=/r/HasA"
-node1_has = call_api(api + query)
+        "rel=/r/PartOf"
+node1_partof = call_api(api + query)
 
-### doing this directly appears not to work for some reason
-##relations = "http://api.conceptnet.io" +\
-##            node1_id +\
-##            "?rel=/r/HasA" +\
-##            "&" +\
-##            "limit=100"
-##node1_has = requests.get(relations).json()
+
+
 
 # next we get the id of something that node1 has
-node2_id = node1_has['edges'][0]['end']['@id']
-
+# later: need to make this so it isn't always index 0, maybe
+# pick based on the weight (how certain it is)
+##node2_hasid = node1_has['edges'][0]['end']['@id']
+node2_partofid = node1_partof['edges'][0]['end']['@id']
 # then we query for things that also have node2
+##query = "/query?" +\
+##        "end=" + node2_hasid +\
+##        "&" +\
+##        "rel=/r/HasA"
+##has_node2 = call_api(api + query)
+#OR
 query = "/query?" +\
-        "end=" + node2_id +\
+        "end=" + node2_partofid +\
         "&" +\
-        "rel=/r/HasA"
-has_node2 = call_api(api + query)
-
+        "rel=/r/PartOf"
+partof_node2 = call_api(api + query)
 # next we get the id of something that has node2
 # and is dissimilar to node1
 # we can't do this too many times lest we overstep the limits
@@ -63,7 +73,19 @@ min_similarity = .05
 max_lookups = 5 #to make sure we don't try too many times
 
 node3_id = ""
-for i, n in enumerate(has_node2['edges'][0:max_lookups]):
+##for i, n in enumerate(has_node2['edges'][0:max_lookups]):
+##    curr_id = n['start']['@id']
+##    related_lookup = "/relatedness?" +\
+##                     "node1=" + node1_id +\
+##                     "&" +\
+##                     "node2=" + curr_id
+##
+##    relatedness = call_api(api + related_lookup)['value']
+##
+##    if min_similarity < relatedness < max_similarity:
+##        node3_id = curr_id
+##        break
+for i, n in enumerate(partof_node2['edges'][0:max_lookups]):
     curr_id = n['start']['@id']
     related_lookup = "/relatedness?" +\
                      "node1=" + node1_id +\
@@ -75,7 +97,6 @@ for i, n in enumerate(has_node2['edges'][0:max_lookups]):
     if min_similarity < relatedness < max_similarity:
         node3_id = curr_id
         break
-
 # finding attribute of node3 that node1 doesn't share
 query = "/query?" +\
         "start=" + node3_id +\
@@ -85,12 +106,16 @@ node3_used = call_api(api + query)
 
 
 # print riddles
-# format: node2_id, node3_used, node1_id
-def print_riddles(has, used, answer):
-    print("What has", has,
+# format: node2_hasid, node3_used, node1_id
+##def print_riddles(has, used, answer):
+##    print("What has", has,
+##          "but isn't used for", used['edges'][0]['end']['@id'],
+##          "??", answer)
+##print_riddles(node2_hasid, node3_used, node1_id)
+def print_riddles(partof, used, answer):
+    print("What is part of", partof,
           "but isn't used for", used['edges'][0]['end']['@id'],
           "??", answer)
-
-print_riddles(node2_id, node3_used, node1_id)
+print_riddles(node2_partofid, node3_used, node1_id)
 
 print(api_calls)
